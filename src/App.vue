07f9293bdeef2fbe53677a52620c3332ddafd106
Alicia Sykes <sykes.alicia@gmail.com>
2018-09-07 14:27:44 +0100

    Made a navbar

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
 <template>
+
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+
+
+	<header>
+		 <h1 class="logo">
+        <router-link to="/">
+          <img src="../public/img/main-icon.png" alt="">
+          Cyber Defence
+        </router-link>
+      </h1>
+      <ul class="main-nav">
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+      </ul>
+	</header> 
+  
   </div>
+
 </template>
 
+
+<style src="typeface-dosis/index.css" />
+
+
+
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+
 </style>
+
+<style src="./assets/navbar-styles.scss" />
+
