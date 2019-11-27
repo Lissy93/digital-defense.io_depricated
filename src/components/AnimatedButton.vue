9552d424dee3e8e9aaabd709178a3042db415315
Alicia Sykes <sykes.alicia@gmail.com>
2019-11-27 17:55:53 +0000

    Made new home banner

---
+++ src/components/AnimatedButton.vue
@@ -0,0 +1,70 @@
+<template>
+  <button class="animated button">
+      <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AnimatedButton extends Vue {}
+</script>
+
+<style scoped lang="scss">
+
+.button {
+    border: 2px solid #031636b0;
+    border-radius: 12px;
+    color: #031636b0;
+    font-size: 1.8rem;
+    margin: 0.5rem;
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    display: inline-block;
+    vertical-align: middle;
+    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+    position: relative;
+    background: #031636b0;
+}
+
+.animated {
+    -webkit-transform: perspective(1px) translateZ(0);
+    transform: perspective(1px) translateZ(0);
+    -webkit-transition-property: color;
+    transition-property: color;
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    transform-origin: 0% 0%;
+}
+.animated:before {
+    background: #6229fe;
+    border-radius: 10px;
+    content: "";
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    -webkit-transform: scaleY(1);
+    transform: scaleY(1);
+    -webkit-transform-origin: 50%;
+    transform-origin: 50%;
+    -webkit-transition-property: transform;
+    transition-property: transform;
+    -webkit-transition-duration: 0.2s;
+    transition-duration: 0.2s;
+    -webkit-transition-timing-function: ease-out;
+    transition-timing-function: ease-out;
+}
+.animated:hover, .animated:focus, .animated:active {
+    color: #6229fe;
+}
+.animated:hover:before, .animated:focus:before, .animated:active:before {
+    -webkit-transform: scaleY(0);
+    transform: scaleY(0);
+}
+
+
+</style>
--- src/components/WelcomeHero.vue
+++ src/components/WelcomeHero.vue
@@ -5,12 +5,16 @@
       <p class="subtitle">
         Guides, tools and knowledge - to help you keep 
         your personal data protected in a dangerous digital world.
-
       </p>
     </div>
     <div class="hero-child main-logo-in-hero">
       <img src="../../public/img/hero-scaled.png" alt=""> 
     </div>
+    <nav>
+      <AnimatedButton>Why?</AnimatedButton>
+      <AnimatedButton>Beginers Checklist</AnimatedButton>
+      <AnimatedButton>Full Checklist</AnimatedButton>
+    </nav>
   </div>
 </template>
 
@@ -18,9 +22,11 @@
 
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import HomeCarousel from '@/components/Carousel.vue';
+import AnimatedButton from '@/components/AnimatedButton.vue';
 
 @Component({
   components: {
+    AnimatedButton,
     HomeCarousel,
   },
 })
@@ -33,45 +39,59 @@ export default class DangerHero extends Vue {}
 
   .welcome-hero{
     width: 100%;
-    display: flex;
-    padding: 4em 2em;
+    display: flex;    
+    padding: 4em 0;
+    flex-flow: wrap;
     background: $gradient;
     color: $blue;
     text-align: left;
+
     h1 {
       text-align: left;
       color: #031636b0;
+      text-shadow: 
+        0 -1px 0 rgba(225, 225, 225, 0.12), 
+        0 0 2px rgba(0, 0, 0, 0.12), 
+        0 2px 4px rgba(0, 0, 0, 0.24);
     }
     .subtitle {
       text-align: left;
       font-size: 2rem;
       color: #03163670
     }
-
-    .hero-child {
-        width: 100%;
-        @media (min-width: 769px) {
-          &:nth-child(1) { width: 65% }
-          &:nth-child(2) { width: 35% }
+    nav {
+      width: 100%;
+      padding: 0 2%;
+      @media (min-width: 769px) {
+        display: flex;
+        flex-direction: row;
+      }
+      @media (max-width: 768px) {
+        text-align: center;
+        button {
+          width: 90%;
         }
-
-      &.main-logo-in-hero{
-          display: flex;
-          justify-content: center;
-          align-items: center;
       }
 
+    }
+
+  .hero-child {
+    width: 100%;
+    padding: 0 5%;
+    @media (min-width: 769px) {
+      padding: 0 2%;
+      &:nth-child(1) { width: 60% }
+      &:nth-child(2) { width: 30% }
+    }
+    &.main-logo-in-hero{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
     img{
       max-width: 350px;
       width: 90%;
       margin: 0 auto;
-    }
-
-    h1 {
-      text-shadow: 
-        0 -1px 0 rgba(225, 225, 225, 0.12), 
-        0 0 2px rgba(0, 0, 0, 0.12), 
-        0 2px 4px rgba(0, 0, 0, 0.24);
     }
   }
 }
