152b1b23250e8a6846909a424b28f1bb7f52f56d
Alicia Sykes <sykes.alicia@gmail.com>
2019-11-28 22:26:09 +0000

    stuff

--- src/components/WelcomeHero.vue
+++ src/components/WelcomeHero.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="welcome-hero">
-    <div class="hero-child">
+    <!-- <div class="particles-js-canvas-el" id="particles-js"></div> -->
+    <div class="hero-child hc1">
       <h1>Digital Defense</h1>
       <p class="subtitle">
         Guides, tools and knowledge - to help you keep 
         your personal data protected in a dangerous digital world.
       </p>
     </div>
-    <div class="hero-child main-logo-in-hero">
+    <div class="hero-child hc2 main-logo-in-hero">
       <img src="../../public/img/hero-scaled.png" alt=""> 
     </div>
     <nav>
@@ -23,24 +24,49 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import HomeCarousel from '@/components/Carousel.vue';
 import AnimatedButton from '@/components/AnimatedButton.vue';
+import 'particles.js'
+
+const particleData = require('./../data/particles-js-config.json');
 
 @Component({
   components: {
     AnimatedButton,
     HomeCarousel,
   },
+  methods: {
+    initParticlesJS() {
+      console.log('Just Initing');
+    },
+  },
+  mounted: ()=> {
+    // Initialize the particles.js on the hero.
+    (<any>window).particlesJS('particles-js', particleData);
+  },
 })
-export default class DangerHero extends Vue {}
+export default class WelcomeHero extends Vue {}
 </script>
 
 <style scoped lang="scss">
 
   @import '../styles/colors';
 
+> * {
+      z-index: 3;
+    }
+  .particles-js-canvas-el{
+    opacity: 0.3;
+    width: 100%;
+    position: absolute;
+    padding: 0;
+    height: 100%;
+    z-index: 1;
+    top: 0;
+    left:0;
+  }
   .welcome-hero{
     width: 100%;
     display: flex;    
-    padding: 4em 0;
+    padding: 2em 0 4em;
     flex-flow: wrap;
     background: $gradient;
     color: $blue;
@@ -80,8 +106,8 @@ export default class DangerHero extends Vue {}
     padding: 0 5%;
     @media (min-width: 769px) {
       padding: 0 2%;
-      &:nth-child(1) { width: 60% }
-      &:nth-child(2) { width: 30% }
+      &.hc1 { width: 60% }
+      &.hc2 { width: 30% }
     }
     &.main-logo-in-hero{
         display: flex;
