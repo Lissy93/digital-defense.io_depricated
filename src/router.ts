a2a1957347b64139ec9a57c0b6cb62e6adb2095e
Alicia Sykes <alicia@futr.ai>
2019-12-23 13:29:22 +0000

    Moved TS declarations to typings dir, updated node-sass, neatened the router file

--- src/router.ts
+++ src/router.ts
@@ -1,18 +1,19 @@
 import Vue from 'vue';
-
 import Router from 'vue-router';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 import Home from './views/Home.vue';
 import Intermediate from './views/Intermediate.vue';
 import FullList from './views/FullList.vue';
 import HaveIBeenHacked from './views/HaveIBeenHacked.vue';
 
-import Loading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/vue-loading.css';
-
 Vue.use(Loading);
-
 Vue.use(Router);
 
+/**
+ * This file manages which components are rendered for a given path
+ * All application routes are defined below
+ */
 const router = new Router({
   mode: 'history',
   routes: [
@@ -26,12 +27,11 @@ const router = new Router({
       name: 'ultimate-list',
       component: FullList,
     },
-    { path: '/the-ultimate-personal-cyber-security-checklist', redirect: { name: 'ultimate-list' }},
-    { path: '/list', redirect: { name: 'ultimate-list' }},
     {
-      path: '/about',
-      name: 'about',
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/the-ultimate-personal-cyber-security-checklist',
+      redirect: { name: 'ultimate-list' }},
+    {
+      path: '/list', redirect: { name: 'ultimate-list' },
     },
     {
       path: '/beginners-checklist',
@@ -52,7 +52,12 @@ const router = new Router({
       path: '/article/:file',
       name: 'article',
       component: () => import(/* webpackChunkName: "article" */ './views/Article.vue'),
-  },
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
   ],
   scrollBehavior(to, from, savedPosition) {
     return { x: 0, y: 0 };
