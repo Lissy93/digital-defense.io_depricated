bf6e64d9f1186b0814f75651c03829fdb01626f1
Alicia Sykes <sykes.alicia@gmail.com>
2020-07-01 18:27:19 +0100

    Starter Header Styles

--- frontend/src/components/structural/NavBar.vue
+++ frontend/src/components/structural/NavBar.vue
@@ -1,30 +1,53 @@
 <template>
+  <header v-click-outside="closeAllDropdowns" class="dd-nav-bar">
 
-  <!-- Site Navigation Link Cointainer -->
-  <nav>
-    <ul class="main-nav" v-bind:class="{ menuOpen: isMenuOpen }">
-      <li
-        v-for="navItem in $static.allDdNavigationMenus.edges"
-        v-bind:key="navItem.node.link.id"
-      >
-        <!-- Navigation Bar Link Item -->
-        <router-link :to="navItem.node.link.url">
-          {{navItem.node.link.display_name}}
-        </router-link>
-        
-        <!-- Optional Drop-down Menu, Visible on Hover -->
-        <ul v-if="navItem.node.children.length > 0">
-          <li 
-            v-for="subNavItem in navItem.node.children"
-            v-bind:key="subNavItem.id"
+    <!-- Burger menu for mobile-->
+    <!-- <Burger class="bigmac" @onBurgerChange="onBurgerChange" :startIsMenuOpen="isMenuOpen"/> -->
+
+    <!-- Main Logo and Title -->
+    <router-link to="/">
+      <h1 class="nav-title">
+          <!-- <img src="../../public/img/hero-scaled.png" alt="" v-click-outside="closeAllDropdowns"> -->
+          Digital Defence
+      </h1>
+    </router-link>
+
+    <!-- Site Navigation Link Cointainer -->
+    <nav>
+      <ul class="main-nav" v-bind:class="{ menuOpen: isMenuOpen }">
+
+        <li
+          v-for="navItem in $static.allDdNavigationMenus.edges"
+          v-bind:key="navItem.node.link.id"
+          @mouseover="visibleDropdown = navItem.node.link.id"
+        >
+          <!-- Navigation Bar Link Item -->
+          <div>
+            <router-link :to="navItem.node.link.url">
+              {{navItem.node.link.display_name}}
+            </router-link>
+          </div>
+          
+          <!-- Optional Drop-down Menu, Visible on Hover -->
+          <ul
+            v-if="navItem.node.children.length > 0"
+            v-bind:class="{ open: visibleDropdown == navItem.node.link.id }"
+            @mouseleave="closeAllDropdowns"
+            class="sub-nav"
           >
-            {{subNavItem.display_name}}
-          </li>
-        </ul>
-      </li>
-    </ul>
+            <li 
+              v-for="subNavItem in navItem.node.children"
+              v-bind:key="subNavItem.id"
+            >
+              {{subNavItem.display_name}}
+            </li>
+          </ul>
+        </li>
+      </ul>
+      
+    </nav>
 
-  </nav>
+  </header>
 </template>
 
 <static-query>
@@ -75,6 +98,71 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+
+header.dd-nav-bar {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5em 1em;
+    background: $primary;
+    list-style: none;
+
+    a {
+      text-decoration: none;
+    }
+
+    /** Title */
+    h1.nav-title {
+      margin: 0;
+      font-size: 2.5rem;
+      font-weight: normal;
+      text-decoration: none;
+      color: $accent1;
+    }
+
+    /** Main Navigation Links, Within Header Bar */
+    nav {
+      ul.main-nav {
+        display: flex;
+        flex-direction: row;
+        list-style: none;
+        padding: 0;
+
+        /** Main Nav Link */
+        li a {
+          display: block;
+          padding: 0.3em 0.6em;
+          margin: 0 0.5rem;
+          min-width: 80px;
+          border: 1px solid $accent1;
+          color: $accent1;
+          line-height: 2;
+          text-decoration: none;
+          font-family: 'Dosis', sans-serif;
+          font-size: 1em;
+          text-align: center;
+          
+          &.router-link-exact-active {
+            border: 1px solid $accent1;
+          }
+          
+          &.router-link-active:hover, &:hover, &.router-link-exact-active {
+            background: $accent1;
+            border: 1px solid $accent1;
+            color: $primary;
+          }
+        }
+
+        /** Sub-Nav */
+        li ul.sub-nav {
+          display: none;
+        }
+
+      }
+    }
+}
 
 </style>
