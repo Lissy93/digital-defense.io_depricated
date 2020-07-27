a890f12f4965335f8861f08c5753bb264d303196
Alicia Sykes <sykes.alicia@gmail.com>
2020-07-27 15:46:26 +0100

    Adds Empty Hero Component

---
+++ frontend/src/components/structural/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="hero">
+        Hero.
+    </header>
+</template>
+
+<static-query>
+
+</static-query>
+
+<script>
+
+export default {
+  name: 'Hero',
+}
+</script>
+
+<style lang="scss">
+.hero {
+    background: #2DD8CF;
+    margin: 0;
+    height: 15rem;
+}
+
+</style>
