a5c222c3fcff0e8fb311aeb8e453b4c05bc4f15c
Alicia Sykes <sykes.alicia@gmail.com>
2020-08-20 21:53:38 +0100

    Demo Buttons in Hero

--- frontend/src/components/structural/Hero.vue
+++ frontend/src/components/structural/Hero.vue
@@ -1,6 +1,9 @@
 <template>
     <header class="hero">
-        Hero.
+        <h2>Tools, Guides and Lists to defend your digital life against threats</h2>
+        <div class="software-list hero-button">Awesome Software List</div>
+        <div class="full-list hero-button">Personal Security Checklist</div>
+        <div class="get-started hero-button">Get Started</div>
     </header>
 </template>
 
@@ -17,9 +20,29 @@ export default {
 
 <style lang="scss">
 .hero {
-    background: #2DD8CF;
+    background: $accent1;
     margin: 0;
     height: 15rem;
+    h2 {
+        margin: 0;
+        padding: 1rem;
+        color: $primary;
+        font-size: 1.8rem;
+    }
+    .hero-button {
+        border: 2px solid $primary;
+        border-radius: 8px;
+        padding: 0.5rem;
+        margin: 0.5rem;
+        display: inline-flex;
+        min-width: 180px;
+        font-size: 1.8rem;
+        &.get-started {
+            background: $primary;
+            color: $accent1;
+
+        }
+    }
 }
 
 </style>
